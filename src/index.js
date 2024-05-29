90e0cf27bb8f168171285f61264164098e2fa493
sumanmc <105421349+sumanmc@users.noreply.github.com>
2024-05-29 18:45:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,12 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Service Worker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      console.log('Service Worker registration failed: ', err);
+    });
+}
 reportWebVitals();
